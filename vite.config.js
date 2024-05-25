2059afaec456c92ff362bc8c7838ce5d681d5eb8
Yared Ayele <yaredayele67@gmail.com>
2024-05-25 02:37:56 -0700

    push into new branch

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CodeMasters-Institute/', // Adjust based on your repository name
-  build: {
-    outDir: 'dist', // or 'build' if that's your preferred directory
-  },
 })
